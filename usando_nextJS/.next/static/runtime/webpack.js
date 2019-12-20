29350e5e58cb0b7ab10e4f8b8e20d30c0560ac4e
brenodev <breno.souza.developer@gmail.com>
2019-12-20 13:28:53 -0300

    realizado requisição com axios e testando o next

--- usando_nextJS/.next/static/runtime/webpack.js
+++ usando_nextJS/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1d87c0002c9105e3cbe4";
+/******/ 	var hotCurrentHash = "3778bcb2660098cf7667";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
