b07affdbb59823cc089ff3d70f9242bb823995f9
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-23 21:35:52 -0300

    adicionando file-loader para imports de imgse configurado para as demais extensoes

--- introducao_ao_reactJS/modulo04/src/App.js
+++ introducao_ao_reactJS/modulo04/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './App.css'
+import banner from './assets/images/tecno.png'
 
 export default function App () {
-  return <h1>Ola mundo</h1>
+  return (
+    <>
+      <h1>Ola mundo</h1>
+      <img src={banner} alt='banner' />
+    </>
+  )
 }
