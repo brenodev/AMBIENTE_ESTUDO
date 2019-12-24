5f56d2f991cd8385e0c7b50ca2fb84d25434fa30
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-23 23:26:47 -0300

    usando componentDidUpdate para realizar verificação no state e armazenar no localStorage

--- introducao_ao_reactJS/modulo04/src/components/techList/index.js
+++ introducao_ao_reactJS/modulo04/src/components/techList/index.js
@@ -4,8 +4,21 @@ import TechItem from "../techItem"
 class TechList extends Component {
   state = {
     newTech: "",
-    techs: ["Node.JS", "React.JS", "React Native", "Javascript"]
+    techs: []
   };
+  // executado assim que o componente aparece em tela
+  componentDidMount(){}
+  /* executado sempre que houver alterações nas props ou no estado, pois ele recebe as props
+  antigas e o estado antigo com prevProps e prevState como parametros*/
+  componentDidUpdate(_, prevState){
+    // salvar no localStorage somente quando muda algo no estado de techs
+    if(prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs))
+    }
+  }
+ //executado quando o componente deixa de existir
+ componentWillUnmount(){}
+
   // armazenando os dados do input
   handleInputChange = e => {
     this.setState({
@@ -39,10 +52,12 @@ class TechList extends Component {
                 onDelete={() => this.handleDelete(tech)}/>
             ))}
           </ul>
+          <h3>Techs</h3>
+          <label htmlFor="addTech">add techs</label>
           <input
             type="text"
             name=""
-            id=""
+            id="addTech"
             value={this.state.newTech}
             onChange={this.handleInputChange}
           />
