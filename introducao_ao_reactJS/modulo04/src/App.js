8ef9b9b0bdf3dd9f7682d6d4f8ae65804ed60f5d
Breno Lopes <breno.souza.developer@gmail.com>
2019-12-23 22:00:52 -0300

    adicionado @babel/plugin-proposal-class-properties para que o componente entenda o estado

--- introducao_ao_reactJS/modulo04/src/App.js
+++ introducao_ao_reactJS/modulo04/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css'
 import banner from './assets/images/tecno.png'
+import TechList from './components/techList'
 
 export default function App () {
   return (
     <>
       <h1>Ola mundo</h1>
-      <img src={banner} alt='banner' />
+      <img className='banner__container' src={banner} alt='banner' />
+      <TechList />
     </>
   )
 }
